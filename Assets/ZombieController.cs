d68e465d738f856d1a35752a2eec5d91b9daf9ad
pzawadzki <admin@machinacyfrowa.pl>
2023-04-21 14:25:53 +0200

    zombie hear

--- Assets/ZombieController.cs
+++ Assets/ZombieController.cs
@@ -21,29 +21,43 @@ public class ZombieController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //flagi
+        bool seesPlayer = false;
+        bool hearsPlayer = false;
+
         RaycastHit hit;
         //wektor prowadz¹cy od zombiaka do gracza
         Vector3 playerVector = player.position - transform.position;
+
         //testowy raycast
-        Debug.DrawRay(transform.position, playerVector, Color.yellow);
+        //Debug.DrawRay(transform.position, playerVector, Color.yellow);
+
         //"wzrok" zombiaka
-        if(Physics.Raycast(transform.position, playerVector, out hit))
+        Physics.Raycast(transform.position, playerVector, out hit);
+        if (hit.collider.gameObject.CompareTag("Player"))
+            seesPlayer = true;
+        
+        //"s³uch" zombiaka
+        //znajdz wszystkow w promieniu 5m
+        Collider[] nearby = Physics.OverlapSphere(transform.position, 5f);
+        foreach(Collider collider in nearby)
         {
-            Debug.Log("Widzê: " + hit.transform.gameObject.name);
+            if(collider.transform.CompareTag("Player"))
+            {
+                //s³yszymy gracza
+                hearsPlayer = true;
+            }
         }
 
-        //transform.LookAt(player.position);
-
-        //transform.Translate(Vector3.forward * Time.deltaTime);
-        if (hit.collider.gameObject.CompareTag("Player"))
+        if(seesPlayer || hearsPlayer)
         {
             agent.destination = player.position;
             agent.isStopped = false;
-        } else
+        }
+        else
         {
             agent.isStopped = true;
         }
-            
     }
     public void ReceiveDamage(int ammount)
     {
